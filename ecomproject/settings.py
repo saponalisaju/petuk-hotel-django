f60304b437f6fe647398656c0b42658c5196df7e
saponalisaju <saponalibpl12486@gmail.com>
2025-12-24 20:49:49 +0600

    ok

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -154,6 +154,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
